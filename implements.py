94e31ce928b18b08b090a23e4850f236388e728e
HaTaeWoo <wth020g@naver.com>
2024-11-14 12:42:23 +0900

    implement alive

--- implements.py
+++ implements.py
@@ -85,4 +85,4 @@ class Ball(Basic):
     def alive(self):
         # ============================================
         # TODO: Implement a service that returns whether the ball is alive or not
-        pass
+        return self.rect.bottom <= config.display_dimension[1]
